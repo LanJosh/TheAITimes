239916f2ef38b51b49a7a6c059a7d8522c68f0da
Joshua Lan <josh.lan9@gmail.com>
2016-11-19 18:46:40 -0500

    v1

--- TheAITimes.py
+++ TheAITimes.py
@@ -1,4 +1,5 @@
 from NYTData import NewYorkTimes
+from Markov import Markov
 """
 Script develops news headlines for a subject using a Markov model
 """
@@ -8,7 +9,10 @@ def main():
   NYT_scraper = NewYorkTimes(api_key)
   subject = input('Enter the subject to generate headlines for')
   data = NYT_scraper.get_text(subject)
-
+  x = Markov(2) 
+  count = input('Enter the number of headlines to generate')
+  for _ in range(count):
+    print(x.generate_text(10).strip('*'))
 
 if __name__ == '__main__':
   main()
